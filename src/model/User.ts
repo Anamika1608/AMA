0d455e005b0654f6f081a706f9ce541ea8cde6be
Anamika Aggarwal <anamikaagg18@gmail.com>
2024-11-04 19:03:41 +0530

    user schema

---
+++ src/model/User.ts
@@ -0,0 +1,75 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// typescript ki string is string
+// mongoose ki string - String
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date
+}
+
+const messageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string,
+    email: string,
+    password: string,
+    verifyCode: string,
+    expirtyDate: Date,
+    isVerified: boolean,
+    isAccesptingMsg: boolean,
+    message: Message[]
+}
+
+const userSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please provide valid email address"]  // yh dekhna pdega
+    },
+    password: {
+        type: String,
+        required: [true, "Passwrod is required"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Code is required"]
+    },
+    expirtyDate: {
+        type: Date,
+        required: [true, "Expiry Date is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAccesptingMsg: {
+        type: Boolean,
+        default : true
+    },
+    message: [messageSchema]
+})
+
+// typescript model - Model - to check if it exist and have model interface
+// normal model - model - to create the new one
+
+const userModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("userSchema"))
+
+export default userModel ;
